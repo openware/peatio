44fef1ddaa95ffab2f712402fde5339699f292cd
emasiuk <toysoldier1927@gmail.com>
2017-12-20 13:38:54 +0200

    deleted error with first nil element

--- app/controllers/private/withdraws/ripples_controller.rb
+++ app/controllers/private/withdraws/ripples_controller.rb
@@ -1,5 +1,10 @@
 module Private::Withdraws
   class RipplesController < ::Private::Withdraws::BaseController
     include ::Withdraws::Withdrawable
+
+    def new
+      @withdraw = Withdraw.new
+    end
+
   end
 end
--- app/controllers/private/withdraws/satoshis_controller.rb
+++ app/controllers/private/withdraws/satoshis_controller.rb
@@ -1,5 +1,10 @@
 module Private::Withdraws
   class SatoshisController < ::Private::Withdraws::BaseController
     include ::Withdraws::Withdrawable
+
+    def new
+      @withdraw = Withdraw.new
+    end
+
   end
 end
