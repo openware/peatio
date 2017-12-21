d8bb4cdc4281d523a796d62ee0d240a70465f1ae
emasiuk <toysoldier1927@gmail.com>
2017-12-21 13:49:21 +0200

    updated code

--- app/controllers/private/withdraws/ripples_controller.rb
+++ app/controllers/private/withdraws/ripples_controller.rb
@@ -1,10 +1,5 @@
 module Private::Withdraws
   class RipplesController < ::Private::Withdraws::BaseController
     include ::Withdraws::Withdrawable
-
-    def new
-      @withdraw = Withdraw.new
-    end
-
   end
 end
--- app/controllers/private/withdraws/satoshis_controller.rb
+++ app/controllers/private/withdraws/satoshis_controller.rb
@@ -1,10 +1,5 @@
 module Private::Withdraws
   class SatoshisController < ::Private::Withdraws::BaseController
     include ::Withdraws::Withdrawable
-
-    def new
-      @withdraw = Withdraw.new
-    end
-
   end
 end
