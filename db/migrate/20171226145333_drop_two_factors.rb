996a769e911efa72023841fa0179f10158a41de4
emasiuk <toysoldier1927@gmail.com>
2017-12-26 17:25:01 +0200

    update migration

--- db/migrate/20171226145333_drop_two_factors.rb
+++ db/migrate/20171226145333_drop_two_factors.rb
@@ -1,5 +1,7 @@
 class DropTwoFactors < ActiveRecord::Migration
   def change
-    drop_table :two_factors
+    if ActiveRecord::Base.connection.data_source_exists? 'two_factors'
+      drop_table :two_factors
+    end
   end
 end
