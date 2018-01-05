2bade75058e11eaeeb2ef73e6d088aaf4f36ee75
Galya Fedorenko <gfedorenko@heliostech.fr>
2018-01-05 18:24:01 +0200

    Fix for document_translations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20251226171150) do
+ActiveRecord::Schema.define(version: 20180105103910) do
 
   create_table "account_versions", force: :cascade do |t|
     t.integer  "member_id",       limit: 4
