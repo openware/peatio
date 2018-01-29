fb789b2d79b8844ec4a98bde6294d5b43fecf72c
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-01-29 14:28:56 +0200

    Added GET /withdraws/addresses api call

--- app/api/api_v2/withdraws.rb
+++ app/api/api_v2/withdraws.rb
@@ -18,5 +18,22 @@ module APIv2
         .per(params[:limit])
         .tap { |q| present q, with: APIv2::Entities::Withdraw }
     end
+
+    desc 'List your withdraw addresses as paginated collection.', scopes: %w[ history ]
+    params do
+      currencies = Currency.all.map(&:code).map(&:upcase)
+      optional :currency, type: String,  values: currencies + currencies.map(&:downcase), desc: "Any supported currencies: #{currencies.join(',')}."
+      optional :page,     type: Integer, default: 1,   integer_gt_zero: true, desc: 'Page number (defaults to 1).'
+      optional :limit,    type: Integer, default: 100, range: 1..1000, desc: 'Number of withdraws per page (defaults to 100, maximum is 1000).'
+    end
+    get '/withdraws/addresses' do
+      current_user
+        .fund_sources
+        .order(id: :desc)
+        .tap { |q| q.where!(currency: params[:currency].downcase) if params[:currency] }
+        .page(params[:page])
+        .per(params[:limit])
+        .tap { |q| present q, with: APIv2::Entities::WithdrawAddress }
+    end
   end
 end
