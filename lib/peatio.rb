c31ec63c92da3981d8cccb29b54074fd11f69257
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-01-31 15:26:34 +0200

    Provide global base_fiat_ccy_sym method

--- lib/peatio.rb
+++ lib/peatio.rb
@@ -1,7 +1,11 @@
 module Peatio
   class << self
     def base_fiat_ccy
-      ENV['BASE_FIAT_CCY']
+      ENV['BASE_FIAT_CCY'].upcase
+    end
+
+    def base_fiat_ccy_sym
+      base_fiat_ccy.to_sym
     end
   end
 end
