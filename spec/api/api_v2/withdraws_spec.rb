fe017bf4cb9dcb742849d4044611ccd8607ee863
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-02-05 19:52:29 +0200

    POST /api/v2/withdraws allows amount to be fraction number (#452)

--- spec/api/api_v2/withdraws_spec.rb
+++ spec/api/api_v2/withdraws_spec.rb
@@ -84,6 +84,11 @@ describe APIv2::Withdraws, type: :request do
       signed_post '/api/v2/withdraws', params: { currency: 'BTC', address_id: btc_withdraw_addresses.first.id, amount: '1' }, token: token
       expect(response.code).to eq '201'
     end
+
+    it 'should allow to create withdraw where amount is fraction number' do
+      signed_post '/api/v2/withdraws', params: { currency: 'BTC', address_id: btc_withdraw_addresses.first.id, amount: '0.1' }, token: token
+      expect(response.code).to eq '201'
+    end
   end
 
   describe 'GET /api/v2/withdraws/addresses' do
