f62be81dcc9db1d667ad7a6f8adf7ae6c3e96437
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-04-03 20:30:17 +0300

    WIP controller for create and new :

--- app/controllers/admin/markets_controller.rb
+++ app/controllers/admin/markets_controller.rb
@@ -5,5 +5,22 @@ module Admin
     def index
       @markets = Market.all.page params[:page]
     end
+
+    def new
+      @deposit = Market.new
+    end
+
+    def create
+      market_params
+      binding.pry
+    end
+
+    private
+    def market_params
+      params.require(:market).permit(:bid_unit)
+    end
+    # def show
+    #   @market = Market.find_by_id(params[:id])
+    # end
   end
 end
