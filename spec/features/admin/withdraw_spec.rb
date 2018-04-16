728f2b9f286a0db755b8c130879cf35a9e58b165
Yaroslav Konoplov <eahome00@gmail.com>
2018-04-16 13:15:27 +0300

    Up!

--- spec/features/admin/withdraw_spec.rb
+++ spec/features/admin/withdraw_spec.rb
@@ -1,38 +1,28 @@
-feature 'withdraw', js: true do
-  let!(:member) { create :member, :verified_identity }
-  let!(:admin_member) { create :member, :verified_identity, email: Member.admins.first }
-
-  let!(:usd_account) do
-    member.get_account(:usd).tap { |a| a.update!(locked: 8000, balance: 10_000) }
-  end
-  let!(:btc_account) do
-    member.get_account(:btc).tap { |a| a.update!(locked: 10, balance: 50) }
-  end
-  let!(:usd_withdraw) { create :usd_withdraw, member: member, sum: 5000, aasm_state: :accepted, account: usd_account }
-  let!(:btc_withdraw) { create :btc_withdraw, member: member, sum: 10, aasm_state: :accepted, account: btc_account }
-
-  before { Withdraw.any_instance.stubs(:validate_password).returns(true) }
+feature 'Withdraw', js: true do
+  let!(:member) { create(:member, :verified_identity) }
+  let!(:admin_member) { create(:member, :verified_identity, email: Member.admins.first) }
+  let!(:usd_account) { member.get_account(:usd).tap { |a| a.update!(locked: 8000, balance: 10_000) } }
+  let!(:btc_account) { member.get_account(:btc).tap { |a| a.update!(locked: 10, balance: 50) } }
+  let!(:usd_withdraw) { create(:usd_withdraw, member: member, sum: 5000, aasm_state: :accepted, account: usd_account) }
+  let!(:btc_withdraw) { create(:btc_withdraw, member: member, sum: 10, aasm_state: :accepted, account: btc_account) }
 
   def visit_admin_withdraw_page
     sign_in admin_member
     click_link admin_member.email
     click_on I18n.t('header.admin')
-
     within '#dashboard-index' do
       expect(page).to have_content(I18n.t('layouts.admin.menus.items.operating.withdraws'))
       click_on I18n.t('layouts.admin.menus.items.operating.withdraws')
     end
   end
 
-
   it 'allows admin to view USD withdraws' do
     visit_admin_withdraw_page
     click_link usd_withdraw.currency.code.upcase
-
     expect(page).to have_content(usd_withdraw.rid)
     expect(page).to have_content(usd_withdraw.amount)
     click_link I18n.t('actions.view')
-    page.within_window(windows.last) do
+    page.within_window windows.last do
       expect(page).to have_content(I18n.t('actions.process'))
       expect(page).to have_content(I18n.t('actions.reject'))
     end
@@ -42,7 +32,7 @@ feature 'withdraw', js: true do
     visit_admin_withdraw_page
     click_link usd_withdraw.currency.code.upcase
     click_link I18n.t('actions.view')
-    page.within_window(windows.last) do
+    page.within_window windows.last do
       click_on I18n.t('actions.process')
       expect(current_path).to eq(admin_withdraw_path(currency: usd_account.currency.code, id: usd_account.id))
     end
@@ -52,11 +42,9 @@ feature 'withdraw', js: true do
 
   it 'allows admin to reject USD withdraw' do
     visit_admin_withdraw_page
-
     click_link usd_withdraw.currency.code.upcase
-
     click_on I18n.t('actions.view')
-    page.within_window(windows.last) do
+    page.within_window windows.last do
       click_on I18n.t('actions.reject')
       expect(current_path).to eq(admin_withdraw_path(currency: usd_account.currency.code, id: usd_account.id))
     end
@@ -67,11 +55,10 @@ feature 'withdraw', js: true do
   it 'allows admin to view BTC withdraws' do
     visit_admin_withdraw_page
     click_link btc_withdraw.currency.code.upcase
-
     expect(page).to have_content(btc_withdraw.rid.truncate(22))
     expect(page).to have_content(btc_withdraw.amount)
     click_link I18n.t('actions.view')
-    page.within_window(windows.last) do
+    page.within_window windows.last do
       expect(page).to have_content(I18n.t('actions.process'))
       expect(page).to have_content(I18n.t('actions.reject'))
     end
@@ -81,11 +68,10 @@ feature 'withdraw', js: true do
     visit_admin_withdraw_page
     click_link btc_withdraw.currency.code.upcase
     click_link I18n.t('actions.view')
-    page.within_window(windows.last) do
+    page.within_window windows.last do
       click_on I18n.t('actions.process')
       expect(current_path).to eq(admin_withdraw_path(currency: btc_account.currency.code, id: btc_account.id))
     end
-
     expect(btc_account.reload.locked).to be_d '10'
     expect(btc_account.reload.balance).to be_d '50'
     expect(btc_withdraw.reload.processing?).to be true
@@ -93,11 +79,9 @@ feature 'withdraw', js: true do
 
   it 'allows admin to reject BTC withdraw' do
     visit_admin_withdraw_page
-
     click_link btc_withdraw.currency.code.upcase
-
     click_on I18n.t('actions.view')
-    page.within_window(windows.last) do
+    page.within_window windows.last do
       click_on I18n.t('actions.reject')
       expect(current_path).to eq(admin_withdraw_path(currency: btc_withdraw.currency.code, id: btc_withdraw.id))
     end
