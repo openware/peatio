054254997633d382aaef91bd7f989e814b8abf33
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-04-16 17:24:13 +0300

    Update specs

--- spec/controllers/admin/currencies_controller_spec.rb
+++ spec/controllers/admin/currencies_controller_spec.rb
@@ -20,22 +20,24 @@ describe Admin::CurrenciesController, type: :controller do
 
   describe '#update' do
     before do
-      valid_currency_attributes.merge! \
-        quick_withdraw_limit:         1000,
-        visible:                      true,
-        base_factor:                  10**6,
-        precision:                    6,
-        api_client:                   'NEW',
-        json_rpc_endpoint:            'http://new.coin',
-        rest_api_endpoint:            'http://api.new.coin',
-        bitgo_test_net:               true,
-        bitgo_wallet_id:              'id',
-        bitgo_wallet_address:         'address',
-        bitgo_wallet_passphrase:      'passphrase',
-        bitgo_rest_api_root:          'http://api.new.coin',
-        bitgo_rest_api_access_token:  'token',
-        wallet_url_template:          'http://new.coin/ad',
-        transaction_url_template:     'http://new.coin/tx'
+      valid_currency_attributes
+        .except!(:code, :type)
+        .merge! \
+          quick_withdraw_limit:         1000,
+          visible:                      true,
+          base_factor:                  10**6,
+          precision:                    6,
+          api_client:                   'NEW',
+          json_rpc_endpoint:            'http://new.coin',
+          rest_api_endpoint:            'http://api.new.coin',
+          bitgo_test_net:               true,
+          bitgo_wallet_id:              'id',
+          bitgo_wallet_address:         'address',
+          bitgo_wallet_passphrase:      'passphrase',
+          bitgo_rest_api_root:          'http://api.new.coin',
+          bitgo_rest_api_access_token:  'token',
+          wallet_url_template:          'http://new.coin/ad',
+          transaction_url_template:     'http://new.coin/tx'
     end
 
     before { request.env['HTTP_REFERER'] = '/admin/currencies' }
