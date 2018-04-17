3afb60c9bc8b4fe0fd74497277cdca4cb42f9378
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-04-17 17:10:06 +0300

    wip

--- db/migrate/20180417111305_migrate_withdraw_channels_to_currency.rb
+++ db/migrate/20180417111305_migrate_withdraw_channels_to_currency.rb
@@ -3,7 +3,6 @@ class MigrateWithdrawChannelsToCurrency < ActiveRecord::Migration
     add_column :currencies, :withdraw_fee, :decimal, null: false, default: 0, precision: 7, scale: 6
     if defined?(Currency) && File.file?('config/withdraw_channels.old.yml')
       (YAML.load_file('config/withdraw_channels.old.yml') || []).each do |channel|
-        next unless channel.key?('fee')
         Currency.find_by_code!(channel.fetch('currency')).tap do |ccy|
           ccy.update_columns(withdraw_fee: channel.fetch('fee'))
         end
