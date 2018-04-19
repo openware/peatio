f73b1f7538df4b8cb52d20336ab8babec2f52580
Yaroslav Konoplov <eahome00@gmail.com>
2018-04-19 18:14:40 +0300

    Up!

--- config/initializers/event_api.rb
+++ config/initializers/event_api.rb
@@ -1,3 +1,4 @@
+require 'active_support/concern'
 require 'active_support/lazy_load_hooks'
 
 module EventAPI
@@ -21,19 +22,72 @@ module EventAPI
     end
 
     def middlewares
-      @middlewares || []
+      @middlewares ||= []
     end
   end
 
   module ActiveRecordExtension
-    def notify()
+    extend ActiveSupport::Concern
 
+    included do
+      # We add «after_commit» callbacks immediately after inclusion.
+      after_commit on: :create, prepend: true do
+        notify_record_created
+      end
+
+      after_commit on: :update, prepend: true do
+        notify_record_updated
+      end
+    end
+
+    module ClassMethods
+      def notifies_about_events(options = {})
+        @event_api_behavior = event_api_behavior.merge(options)
+      end
+
+      def event_api_behavior
+        @event_api_behavior || superclass.instance_variable_get(:@event_api_behavior) || {}
+      end
+    end
+
+    def to_event_api_payload
+      as_json
+    end
+
+    def notify(partial_event_name, event_payload)
+      tokens = ['model']
+      tokens << self.class.event_api_behavior.fetch(:prefix) { self.class.name.underscore.gsub(/\//, '_') }
+      tokens << partial_event_name.to_s
+      full_event_name = tokens.join('.')
+      EventAPI.notify(full_event_name, event_payload)
+    end
+
+    def notify_record_created
+      notify(:created, record: to_event_api_payload)
+    end
+
+    def notify_record_updated
+      current_record  = self
+      previous_record = dup
+      previous_changes.each { |attribute, values| previous_record.send("#{attribute}=", values.first) }
+
+      previous_record.created_at ||= current_record.created_at
+
+      before = previous_record.to_event_api_payload.compact
+      after  = current_record.to_event_api_payload.compact
+
+      notify :updated, \
+        record:  after,
+        changes: before.delete_if { |attribute, value| after[attribute] == value }
     end
   end
 
   module Middlewares
     class IncludeEventMetadata
-
+      def call(event_name, event_payload, options)
+        event_payload[:name] = event_name
+        [event_name, event_payload, options]
+      end
     end
 
     class GenerateJWT
@@ -43,38 +97,47 @@ module EventAPI
     end
 
     class PrintToScreen
-      def call
-
+      def call(event_name, event_payload, options = {})
+        Rails.logger.debug do
+          ['',
+           'Produced new event at ' + Time.current.to_s + ': ',
+           'name    = ' + event_name,
+           'payload = ' + event_payload.to_json,
+           ''].join("\n")
+        end
+        [event_name, event_payload, options]
       end
     end
 
-    class PublishToRabbitMQ
-      def call
+    class PublishToAbstractRabbitMQ
+      def call(event_name, event_payload, options)
+        Rails.logger.debug do
+          ['',
+           'Published new message to RabbitMQ (abstractly):',
+           'exchange    = ' + exchange_name(event_name),
+           'routing key = ' + routing_key(event_name),
+           'payload     = ' + event_payload.to_json,
+           ''
+          ].join("\n")
+        end
+        [event_name, event_payload, options]
+      end
 
+    private
+      # TODO: Validate.
+      def exchange_name(event_name)
+        "peatio.events.#{event_name.split('.').first}"
+      end
+
+      def routing_key(event_name)
+        event_name.split('.').drop(1).join('.')
       end
     end
   end
 
-  class MetadataMiddleware
-    def call(event_name, event_payload, options = {})
-      [event_name, event_payload, options]
-    end
-  end
-
-  class RabbitMQMiddleware
-    def call(event_name, event_payload, options = {})
-      [event_name, event_payload, options]
-    end
-  end
-
-  class LoggingMiddleware
-    def call(event_name, event_payload, options = {})
-      Rails.logger.debug do
-        ['', event_name, '', event_payload.to_json, ''].join("\n")
-      end
-      [event_name, event_payload, options]
-    end
-  end
+  middlewares << Middlewares::IncludeEventMetadata.new
+  middlewares << Middlewares::PrintToScreen.new
+  middlewares << Middlewares::PublishToAbstractRabbitMQ.new
 end
 
 ActiveSupport.on_load(:active_record) { ActiveRecord::Base.include EventAPI::ActiveRecordExtension }
