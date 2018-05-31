3c056262ac7460d5e3eb923bca6a250c61038104
Maksim Litvinov <maksim-litvinov@users.noreply.github.com>
2018-05-31 12:22:10 +0300

    Review fixes

--- app/controllers/public/health_controller.rb
+++ app/controllers/public/health_controller.rb
@@ -4,17 +4,17 @@
 module Public
   class HealthController < ActionController::Base
     def alive
-      render_health_status HealthChecker.alive
+      render_health_status Services::HealthChecker.alive?
     end
 
     def ready
-      render_health_status HealthChecker.ready
+      render_health_status Services::HealthChecker.ready?
     end
 
     private
 
     def render_health_status(health)
-      head(health ? 200 : 503)
+      head health ? 200 : 503
     end
   end
 end
