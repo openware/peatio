6110c6bd29e59c9574c99c6236630cc2c27a6664
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-07-05 15:26:04 +0300

    Rebase 2

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -52,7 +52,7 @@ FactoryBot.define do
         api_client:                       'DASH',
         json_rpc_endpoint:                'http://127.0.0.1:19999',
         deposit_confirmations:            1,
-        withdraw_confirmations: 1,
+        withdraw_confirmations:           1,
         case_sensitive:                   true,
         supports_hd_protocol:             true,
         allow_multiple_deposit_addresses: true
@@ -71,7 +71,7 @@ FactoryBot.define do
         wallet_url_template:              'https://rinkeby.etherscan.io/address/#{address}',
         transaction_url_template:         'https://rinkeby.etherscan.io/tx/#{txid}',
         deposit_confirmations:            1,
-        withdraw_confirmations:   1,
+        withdraw_confirmations:           1,
         case_sensitive:                   false,
         supports_hd_protocol:             false,
         allow_multiple_deposit_addresses: false
@@ -90,7 +90,7 @@ FactoryBot.define do
         wallet_url_template:              'https://bithomp.com/explorer/#{txid}',
         transaction_url_template:         'https://bithomp.com/explorer/#{address}',
         deposit_confirmations:            1,
-        withdraw_confirmations:   1,
+        withdraw_confirmations:           1,
         case_sensitive:                   true,
         supports_hd_protocol:             false,
         allow_multiple_deposit_addresses: false
@@ -110,7 +110,7 @@ FactoryBot.define do
         transaction_url_template:         'https://etherscan.io/tx/#{txid}',
         erc20_contract_address:           '0x87099adD3bCC0821B5b151307c147215F839a110',
         deposit_confirmations:            1,
-        withdraw_confirmations:   1,
+        withdraw_confirmations:           1,
         case_sensitive:                   false,
         supports_hd_protocol:             false,
         allow_multiple_deposit_addresses: false
@@ -129,7 +129,7 @@ FactoryBot.define do
         wallet_url_template:              'https://www.blocktrail.com/tBCC/address/#{address}',
         transaction_url_template:         'https://www.blocktrail.com/tBCC/tx/#{txid}',
         deposit_confirmations:            1,
-        withdraw_confirmations:     1,
+        withdraw_confirmations:           1,
         case_sensitive:                   true,
         supports_cash_addr_format:        true,
         supports_hd_protocol:             true,
