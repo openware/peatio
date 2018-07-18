0feb6508adac2accb698b0ce281a382731e76c4a
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-07-18 15:49:01 +0300

    Use min_confirmations instead of withdraw & deposit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716115113) do
+ActiveRecord::Schema.define(version: 20180718113111) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "member_id",   limit: 4,                                          null: false
