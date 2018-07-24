b71893589c938f9dd6bc85d765778184b1dfd2da
Ritesh Chohda <ritesh.skyach@gmail.com>
2018-07-24 18:57:16 +0530

    Added Dealloc Prepare

--- db/migrate/20180716110346_add_archive_orders_stored_procedure.rb
+++ db/migrate/20180716110346_add_archive_orders_stored_procedure.rb
@@ -8,12 +8,14 @@ class AddArchiveOrdersStoredProcedure < ActiveRecord::Migration
       SET @prepare_table = CONCAT('CREATE TABLE IF NOT EXISTS ', @table_name, ' SELECT * FROM orders WHERE 1=0');
       PREPARE create_table from @prepare_table;
       EXECUTE create_table;
+      DEALLOCATE PREPARE create_table;
       SET @insert_date = CONCAT('INSERT INTO ', @table_name,
       ' SELECT * FROM orders WHERE DATE(updated_at) BETWEEN ', DATE_FORMAT(NOW() ,'%Y-%m-01'),
       ' AND ', CURDATE() - INTERVAL 1 DAY,
       ' AND state = 200' );
       PREPARE insert_stmt from @insert_date;
       EXECUTE insert_stmt;
+      DEALLOCATE PREPARE insert_stmt;
       DELETE FROM orders WHERE DATE(updated_at) BETWEEN DATE_FORMAT(NOW() ,'%Y-%m-01') AND CURDATE() - INTERVAL 1 DAY AND state = 200;
     END
     SQL
