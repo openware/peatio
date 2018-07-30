b8eb954dc1dfbbf3178a4bfee2444bc67a444423
Yehor Chumak <ychumak@heliostech.fr>
2018-07-30 16:49:13 +0300

    Add icon_url in api currency entities. Minor related refactoring

--- app/helpers/currency_helper.rb
+++ app/helpers/currency_helper.rb
@@ -6,28 +6,21 @@ module CurrencyHelper
   # since Gon::Jbuilder attaches ActionView::Helpers which behave differently
   # compared to what ActionController does.
   def currency_icon_url(currency)
-    if currency.coin?
-      ActionController::Base.helpers.image_url coin_icon_url(currency)
-    else
-      ActionController::Base.helpers.image_url fiat_icon_url(currency)
-    end
+      ActionController::Base.helpers.image_url currency_icon_url(currency)
   end
 
   private
 
-  def coin_icon_url(currency)
+  def currency_icon_url(currency)
     if currency.icon_url.blank?
-      "yarn_components/cryptocurrency-icons/svg/color/#{currency.code}.svg"
+      if currency.coin?
+        "yarn_components/cryptocurrency-icons/svg/color/#{currency.code}.svg"
+      else
+        "yarn_components/currency-flags/src/flags/#{currency.code}.png"
+      end
     else
       currency.icon_url
     end
   end
 
-  def fiat_icon_url(currency)
-    if currency.icon_url.blank?
-      "yarn_components/currency-flags/src/flags/#{currency.code}.png"
-    else
-      currency.icon_url
-    end
-  end
 end
