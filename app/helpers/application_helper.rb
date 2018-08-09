84a29beed3e80caf862ab8e1305ef1b136650693
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-08-09 18:43:43 +0300

    Fixed icons (#1516)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -112,10 +112,6 @@ module ApplicationHelper
   # since Gon::Jbuilder attaches ActionView::Helpers which behave differently
   # compared to what ActionController does.
   def currency_icon_url(currency)
-    if currency.coin?
-      ActionController::Base.helpers.image_url "yarn_components/cryptocurrency-icons/svg/color/#{currency.code}.png"
-    else
-      ActionController::Base.helpers.image_url "yarn_components/currency-flags/src/flags/#{currency.code}.png"
-    end
+    ActionController::Base.helpers.image_url "assets/#{currency.code}.svg"
   end
 end
