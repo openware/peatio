b00a57023e4806a8cbf5b40d39ed035da47c4b84
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-10-27 22:50:20 +0300

    Ability to get accounts summary

--- app/services/accounting_service.rb
+++ app/services/accounting_service.rb
@@ -5,12 +5,41 @@ module AccountingService
   Error = Class.new(StandardError)
 
   ZERO = 0.to_d
+
   class << self
     def find_or_create_for(owner, currency_id)
       "AccountingService::#{owner.type.capitalize}Entry"
         .constantize
         .new(owner: owner, currency_id: currency_id)
         .tap(&:initialize_accounts!)
+    end
+
+    # TODO: Method should be moved to PlatformEntry.
+    # And calculated as balance in assets account.
+    def assets_for(currency_id)
+      currency_type = Currency.find(currency_id).type.to_sym
+      codes = Chart.where(
+        currency_type: currency_type,
+        type:          :liabilities,
+        kind:          :main
+      ).map(&:code)
+      accounts_ids = Account.where(currency_id: currency_id, code: codes).pluck(:id)
+      operations = Operation.where(account_id: accounts_ids)
+      operations.sum(:credit) - operations.sum(:debit)
+    end
+
+    # TODO: Method should be moved to PlatformEntry.
+    # And calculated as balance in locked assets account.
+    def locked_assets_for(currency_id)
+      currency_type = Currency.find(currency_id).type.to_sym
+      codes = Chart.where(
+        currency_type: currency_type,
+        type:          :liabilities,
+        kind:          :locked
+      ).map(&:code)
+      accounts_ids = Account.where(currency_id: currency_id, code: codes).pluck(:id)
+      operations = Operation.where(account_id: accounts_ids)
+      operations.sum(:credit) - operations.sum(:debit)
     end
   end
 end
