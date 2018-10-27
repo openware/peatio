5ac13ae3ae7b980e88fc2ff2b42ae72a9b8c5d79
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-10-27 23:43:17 +0300

    AccountingService::MemberEntry operations specs

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,79 +5,7 @@ describe Account do
   let(:initial_balance) { 10.0 }
   let(:initial_locked) { 10.0 }
 
-  subject { create_account(:btc, balance: initial_balance, locked: initial_locked) }
-
-  it 'sums locked and balance' do
-    expect(subject.amount).to be_d initial_balance + initial_locked
-  end
-
-  context 'operation amount less than account balance' do
-    let(:deposit) { create(:deposit_btc, amount: 1, member: subject.member) }
-    let(:withdraw) { create(:btc_withdraw, sum: 0.5, member: subject.member) }
-
-    it 'creates operation and debit funds' do
-      expect{
-        subject.sub_funds(withdraw.sum, withdraw)
-      }.to change{ subject.operations.count }
-
-      expect(subject.balance).to eql(initial_balance - withdraw.sum.to_d)
-    end
-
-    it 'creates operation and credit funds' do
-      expect{
-        subject.plus_funds(deposit.amount, deposit)
-      }.to change{ subject.operations.count }
-
-      expect(subject.balance).to eql(initial_balance + deposit.amount.to_d)
-    end
-
-    # it { expect(subject.unlock_funds('1.0'.to_d, reference).locked).to eql '9.0'.to_d }
-    # it { expect(subject.unlock_funds('1.0'.to_d).balance).to eql '11.0'.to_d }
-    # it { expect(subject.lock_funds('1.0'.to_d).locked).to eql '11.0'.to_d }
-    # it { expect(subject.lock_funds('1.0'.to_d).balance).to eql '9.0'.to_d }
-  end
-
-  it { expect(subject.unlock_and_sub_funds('1.0'.to_d).balance).to be_d '10' }
-  it { expect(subject.unlock_and_sub_funds('1.0'.to_d).locked).to be_d '9' }
-
-  it { expect(subject.sub_funds('0.1'.to_d).balance).to eql '9.9'.to_d }
-  it { expect(subject.plus_funds('0.1'.to_d).balance).to eql '10.1'.to_d }
-  it { expect(subject.unlock_funds('0.1'.to_d).locked).to eql '9.9'.to_d }
-  it { expect(subject.unlock_funds('0.1'.to_d).balance).to eql '10.1'.to_d }
-  it { expect(subject.lock_funds('0.1'.to_d).locked).to eql '10.1'.to_d }
-  it { expect(subject.lock_funds('0.1'.to_d).balance).to eql '9.9'.to_d }
-
-  it { expect(subject.sub_funds('10.0'.to_d).balance).to eql '0.0'.to_d }
-  it { expect(subject.plus_funds('10.0'.to_d).balance).to eql '20.0'.to_d }
-  it { expect(subject.unlock_funds('10.0'.to_d).locked).to eql '0.0'.to_d }
-  it { expect(subject.unlock_funds('10.0'.to_d).balance).to eql '20.0'.to_d }
-  it { expect(subject.lock_funds('10.0'.to_d).locked).to eql '20.0'.to_d }
-  it { expect(subject.lock_funds('10.0'.to_d).balance).to eql '0.0'.to_d }
-
-  it { expect { subject.sub_funds('11.0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.lock_funds('11.0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.unlock_funds('11.0'.to_d) }.to raise_error(Account::AccountError) }
-
-  it { expect { subject.sub_funds('-1.0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.plus_funds('-1.0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.lock_funds('-1.0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.unlock_funds('-1.0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.sub_funds('0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.plus_funds('0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.lock_funds('0'.to_d) }.to raise_error(Account::AccountError) }
-  it { expect { subject.unlock_funds('0'.to_d) }.to raise_error(Account::AccountError) }
-
-  describe 'double operation' do
-    let(:strike_volume) { '10.0'.to_d }
-    let(:account) { create_account }
-
-    it 'expect double operation funds' do
-      expect do
-        account.plus_funds(strike_volume)
-        account.sub_funds(strike_volume)
-      end.to_not(change { account.balance })
-    end
-  end
+  subject { create_account(:btc) }
 
   describe '#payment_address' do
     it { expect(subject.payment_address).not_to be_nil }
@@ -85,26 +13,6 @@ describe Account do
     context 'fiat currency' do
       subject { create_account(:usd).payment_address }
       it { is_expected.to be_nil }
-    end
-  end
-
-  describe 'concurrent lock_funds' do
-    it 'should raise error on the second lock_funds' do
-      account1 = Account.find subject.id
-      account2 = Account.find subject.id
-
-      expect(subject.reload.balance).to eq BigDecimal.new('10')
-
-      expect do
-        ActiveRecord::Base.transaction do
-          account1.lock_funds(8)
-        end
-        ActiveRecord::Base.transaction do
-          account2.lock_funds(8)
-        end
-      end.to raise_error(Account::AccountError) { |e| expect(e.message).to eq 'Cannot lock funds (amount: 8).' }
-
-      expect(subject.reload.balance).to eq BigDecimal.new('2')
     end
   end
 
@@ -126,7 +34,7 @@ describe Account do
   end
 
   describe '#payment_address!' do
-    it 'returns the same payment address is address generation process is in progress' do
+    it 'returns the same payment address if address generation is in progress' do
       expect(subject.payment_address!).to eq subject.payment_address
     end
 
