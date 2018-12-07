fdf4a57fb64e6d550a6228292b6651dc87e9a435
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-12-07 12:25:59 +0200

    Ability to set custom gas_limit & gas_price per currency.

--- db/schema.rb
+++ db/schema.rb
@@ -238,7 +238,7 @@ ActiveRecord::Schema.define(version: 20181210162905) do
 
   create_table "wallets", force: :cascade do |t|
     t.string   "blockchain_key", limit: 32
-    t.string   "currency_id",    limit: 10
+    t.string   "currency_id",    limit: 5
     t.string   "name",           limit: 64
     t.string   "address",        limit: 255,                                           null: false
     t.integer  "kind",           limit: 4,                                             null: false
