fbb55858f7e4fb9bd016826a88bf65cc5f07653d
Elias Rodrigues <elias.rodrigues@usp.br>
2019-03-27 18:01:26 -0300

    Add publisher_thread to Matching::OrderBookManager
    Upon initialization, OrderBookManager will launch a thread
    that every 0.5 seconds will:
    - try to write its orderbooks' depth to cache
      (only writes if they've been updated)
    - then trigger Peatio::MQ:Events publish for its market data if cache was
      written or 30 seconds have passed with no updates
    
    Also, a finalize method is added to kill the publisher_thread so
    it does not leak.

--- app/trading/matching/order_book_manager.rb
+++ app/trading/matching/order_book_manager.rb
@@ -1,10 +1,11 @@
 # encoding: UTF-8
 # frozen_string_literal: true
+require "peatio/mq/events"
 
 module Matching
   class OrderBookManager
-
-    attr :ask_orders, :bid_orders
+    attr_reader :ask_orders,
+                :bid_orders
 
     def self.build_order(attrs)
       attrs.symbolize_keys!
@@ -19,6 +20,12 @@ module Matching
       @market     = market
       @ask_orders = OrderBook.new(market, :ask, options)
       @bid_orders = OrderBook.new(market, :bid, options)
+
+      @publisher_thread = launch_publisher_thread
+    end
+
+    def finalize
+      @publisher_thread.exit
     end
 
     def get_books(type)
@@ -30,5 +37,43 @@ module Matching
       end
     end
 
+    private
+
+    def launch_publisher_thread
+      @global_market = Global[@market]
+      @triggered_publisher_at = 0
+
+      Thread.new do
+        loop do
+          sleep 0.5
+          begin
+            trigger_publisher if update_cache || max_time_without_update
+          rescue StandardError => error
+            # TODO proper error logging
+          ensure
+            next
+          end
+        end
+      end
+    end
+
+    def trigger_publisher
+      Peatio::MQ::Events.publish("public", @market, "update", {
+        asks: @global_market.asks,
+        bids: @global_market.bids,
+      })
+      @triggered_publisher_at = Time.now.to_i
+    end
+
+    def update_cache
+      depth_asks_updated = @ask_orders.write_depth_to_cache
+      depth_bids_updated = @bid_orders.write_depth_to_cache
+
+      depth_asks_updated || depth_bids_updated
+    end
+
+    def max_time_without_update
+      Time.now.to_i - @triggered_publisher_at > 30 # seconds
+    end
   end
 end
