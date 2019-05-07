8c6336146cbac0720bc4e4dd077f89b19532e3c9
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-05-07 13:08:54 +0300

    Refactor Ethereum1 -> Ethereum

--- config/initializers/blockchain_api.rb
+++ config/initializers/blockchain_api.rb
@@ -1,3 +1,3 @@
 Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain.new
-Peatio::Blockchain.registry[:geth] = Ethereum1::Blockchain.new
-Peatio::Blockchain.registry[:parity] = Ethereum1::Blockchain.new
+Peatio::Blockchain.registry[:geth] = Ethereum::Blockchain.new
+Peatio::Blockchain.registry[:parity] = Ethereum::Blockchain.new
