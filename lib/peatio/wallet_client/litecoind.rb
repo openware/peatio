55f995458851f525bbe6abf0dda9a3b9b76be0fd
FairExchange <36345983+Fair-Exchange@users.noreply.github.com>
2019-05-07 17:41:15 -0600

    Disable Litecoin Legacy withdraws (conflict with BTC)
    Prevent people from getting #rekt when they accidentally withdraw LTC to a BTC address

--- lib/peatio/wallet_client/litecoind.rb
+++ lib/peatio/wallet_client/litecoind.rb
@@ -4,5 +4,16 @@
 module WalletClient
   class Litecoind < Bitcoind
 
+    def inspect_address!(address)
+      json_rpc(:validateaddress, [normalize_address(address)]).fetch('result').yield_self do |x|
+        if normalize_address(address).start_with?('L','M')
+           { address: normalize_address(address), is_valid: !!x['isvalid'] }
+        else
+          { address: normalize_address(address), is_valid: false }
+        end
+      end
+    end
+
+    
   end
 end
