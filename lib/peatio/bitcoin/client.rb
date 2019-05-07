32a564b27943b304549c81ca17788c78327f9e7f
mnaichuk <mnaichuk@heliostech.fr>
2019-05-07 11:50:51 +0300

    Add connection error for bitcoin and ethereum client

--- lib/peatio/bitcoin/client.rb
+++ lib/peatio/bitcoin/client.rb
@@ -1,6 +1,7 @@
 module Bitcoin
   class Client
     Error = Class.new(StandardError)
+    class ConnectionError < Error; end
 
     class ResponseError < Error
       def initialize(code, msg)
@@ -30,7 +31,7 @@ module Bitcoin
       response['error'].tap { |error| raise ResponseError.new(error['code'], error['message']) if error }
       response.fetch('result')
     rescue Faraday::Error => e
-      raise Error, e
+      raise ConnectionError, e
     end
 
     private
