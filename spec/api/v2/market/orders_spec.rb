a3e2446d3ed77828cfb44db5c22e125a8ea6a2af
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-05-22 16:50:41 +0300

    Fix specs

--- spec/api/v2/market/orders_spec.rb
+++ spec/api/v2/market/orders_spec.rb
@@ -297,6 +297,9 @@ describe API::V2::Market::Orders, type: :request do
 
       it 'should cancel specified order' do
         AMQPQueue.expects(:enqueue).with(:matching, action: 'cancel', order: order.to_matching_attributes)
+        AMQPQueue.expects(:enqueue).with(:events_processor,
+                                         subject: :stop_order,
+                                         payload: order.as_json_for_events_processor)
         expect do
           api_post "/api/v2/market/orders/#{order.id}/cancel", token: token
           expect(response).to be_successful
@@ -327,6 +330,9 @@ describe API::V2::Market::Orders, type: :request do
     it 'should cancel all my orders' do
       member.orders.each do |o|
         AMQPQueue.expects(:enqueue).with(:matching, action: 'cancel', order: o.to_matching_attributes)
+        AMQPQueue.expects(:enqueue).with(:events_processor,
+                                         subject: :stop_order,
+                                         payload: o.as_json_for_events_processor)
       end
 
       expect do
@@ -341,6 +347,9 @@ describe API::V2::Market::Orders, type: :request do
     it 'should cancel all my orders for specific market' do
       member.orders.where(market: 'btceth').each do |o|
         AMQPQueue.expects(:enqueue).with(:matching, action: 'cancel', order: o.to_matching_attributes)
+        AMQPQueue.expects(:enqueue).with(:events_processor,
+                                         subject: :stop_order,
+                                         payload: o.as_json_for_events_processor)
       end
 
       expect do
@@ -355,6 +364,9 @@ describe API::V2::Market::Orders, type: :request do
     it 'should cancel all my asks' do
       member.orders.where(type: 'OrderAsk').each do |o|
         AMQPQueue.expects(:enqueue).with(:matching, action: 'cancel', order: o.to_matching_attributes)
+        AMQPQueue.expects(:enqueue).with(:events_processor,
+                                         subject: :stop_order,
+                                         payload: o.as_json_for_events_processor)
       end
 
       expect do
