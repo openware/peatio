ec8931508a202d02fb880c78e038648b447a2ef9
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-07-16 15:46:19 +0300

    Reset changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,14 +21,14 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :redis_store
+    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :redis_store
+    config.cache_store = :null_store
   end
 
   # Print deprecation notices to the Rails logger.
