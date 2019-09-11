0d169caacc17a20e5cf1a97cc3c1d75e03db6d4c
Denis <dfedorchenko@heliostech.fr>
2019-09-11 17:33:22 +0300

    Add details about fees in trade API

--- app/api/v2/admin/entities/trade.rb
+++ app/api/v2/admin/entities/trade.rb
@@ -18,14 +18,6 @@ module API
           ) { |trade| trade.maker.email }
 
           expose(
-            :taker_order_email,
-            documentation: {
-              type: String,
-              desc: 'Trade taker member email.'
-            }
-          ) { |trade| trade.taker.email }
-
-          expose(
             :maker_uid,
             documentation: {
               type: String,
@@ -34,12 +26,86 @@ module API
           ) { |trade| trade.maker.uid }
 
           expose(
+            :maker_fee,
+            documentation: {
+              type: BigDecimal,
+              desc: 'Trade maker fee percentage.',
+            },
+            if: ->(object, options) { options[:extended] }
+          ) { |trade| trade.maker_order.maker_fee }
+
+          expose(
+            :maker_fee_amount,
+            documentation: {
+              type: BigDecimal,
+              desc: 'Trade maker fee amount.',
+            }
+          ) { |trade| fee_amount(trade, trade.maker_order) }
+
+          expose(
+            :maker_currency,
+            documentation: {
+              type: String,
+              desc: 'Trade maker currency code.'
+            }
+          ) { |trade| trade.maker_order.currency.code }
+
+          expose(
+            :maker_order,
+            using: API::V2::Admin::Entities::Order,
+            if: ->(object, options) { options[:extended] }
+          )
+
+          expose(
+            :taker_order_email,
+            documentation: {
+              type: String,
+              desc: 'Trade taker member email.'
+            }
+          ) { |trade| trade.taker.email }
+
+          expose(
             :taker_uid,
             documentation: {
               type: String,
               desc: 'Trade taker member uid.'
             }
           ) { |trade| trade.taker.uid }
+
+          expose(
+            :taker_currency,
+            documentation: {
+              type: String,
+              desc: 'Trade taker currency code.'
+            }
+          ) { |trade| trade.taker_order.currency.code }
+
+          expose(
+            :taker_fee,
+            documentation: {
+              type: BigDecimal,
+              desc: 'Trade taker fee percentage.',
+            },
+            if: ->(object, options) { options[:extended] }
+          ) { |trade| trade.taker_order.taker_fee }
+
+          expose(
+            :taker_fee_amount,
+            documentation: {
+              type: BigDecimal,
+              desc: 'Trade taker fee amount.',
+            }
+          ) { |trade| fee_amount(trade, trade.taker_order) }
+
+          expose(
+            :taker_order,
+            using: API::V2::Admin::Entities::Order,
+            if: ->(object, options) { options[:extended] }
+          )
+
+          def fee_amount(trade, order)
+            trade.order_fee(order) * (order.side == 'buy' ? trade.amount : trade.total)
+          end
         end
       end
     end
