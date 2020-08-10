d60cee5b7f0c0d71374f1e423f69fa804066f852
Chumak Nadiia <chumaknadya19@gmail.com>
2020-08-10 11:39:28 +0300

    Feature: Add ability to edit permissions by field

--- app/api/v2/admin/members.rb
+++ app/api/v2/admin/members.rb
@@ -72,7 +72,7 @@ module API
                    desc: 'User gruop'
         end
         put '/members/:uid' do
-          admin_authorize! :update, Member
+          admin_authorize! :update, Member, params[:group]
           declared_params = declared(params)
 
           member = Member.find_by!(uid: declared_params[:uid])
