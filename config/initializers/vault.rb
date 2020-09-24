46aa74a0f7379948b7044a08d0ae361b31fac16d
Camille Meulien <cmeulien@heliostech.fr>
2020-09-24 17:46:48 +0300

    Improve the vault token renew process
     * Add logs displaying vault token renew process status
     * Fails from the start if something is wrong in production

--- config/initializers/vault.rb
+++ config/initializers/vault.rb
@@ -4,28 +4,45 @@
 require 'vault/totp'
 require 'vault/rails'
 
-Vault.configure do |config|
-  config.enabled = Rails.env.production?
-  config.address = ENV.fetch('VAULT_URL', 'http://127.0.0.1:8200')
-  config.token = ENV.fetch('VAULT_TOKEN')
-  config.ssl_verify = false
-  config.timeout = 60
-  config.application = ENV.fetch('VAULT_APP_NAME', 'peatio')
-end
-
-if Rails.env.production? && ENV.fetch('VAULT_RENEW', false)
-  def renew_process
-    token = Vault.auth_token.lookup(Vault.token)
-    sleep(token.data[:ttl] * (1 + rand) * 0.1)
-    Vault.auth_token.renew(token.data[:id])
+if ENV['VAULT_TOKEN'].to_s != ''
+  Vault::Rails.configure do |config|
+    config.enabled = Rails.env.production?
+    config.address = ENV.fetch('VAULT_URL', 'http://127.0.0.1:8200')
+    config.token = ENV.fetch('VAULT_TOKEN')
+    config.ssl_verify = false
+    config.timeout = 60
+    config.application = ENV.fetch('VAULT_APP_NAME', 'peatio')
   end
 
-  Thread.new do
-    loop do
-      renew_process
-    rescue StandardError => e
-      report_exception(e)
-      sleep 60
+  def renew_process
+    puts 'renew_process'
+    token = Vault.auth_token.lookup(Vault.token)
+    time = token.data[:ttl] * (1 + rand) * 0.1
+    Rails.logger.debug '[VAULT] Token will renew in %.0f sec' % time
+    sleep(time)
+    Vault.auth_token.renew(token.data[:id])
+    Rails.logger.info '[VAULT] Token renewed'
+  end
+
+  token = Vault.auth_token.lookup(Vault.token)
+
+  if token.data[:renewable]
+    Rails.logger.info '[VAULT] Starting token renew thread'
+    Thread.new do
+      loop do
+        renew_process
+      rescue StandardError => e
+        report_exception(e)
+        sleep 60
+      end
     end
+  else
+    Rails.logger.info '[VAULT] Token is not renewable'
+  end
+else
+  if Rails.env.production?
+    raise 'Environment variable VAULT_TOKEN is missing'
+  else
+    Rails.logger.warn 'Environment variable VAULT_TOKEN is missing'
   end
 end
