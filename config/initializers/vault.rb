82da26736c449410b58a8050b9423992f4a49318
Camille Meulien <cmeulien@heliostech.fr>
2020-10-01 18:50:01 +0300

    Improve vault initializer

--- config/initializers/vault.rb
+++ config/initializers/vault.rb
@@ -4,16 +4,16 @@
 require 'vault/totp'
 require 'vault/rails'
 
-if ENV['VAULT_TOKEN'].to_s != ''
-  Vault::Rails.configure do |config|
-    config.enabled = Rails.env.production?
-    config.address = ENV.fetch('VAULT_ADDR', 'http://127.0.0.1:8200')
-    config.token = ENV.fetch('VAULT_TOKEN')
-    config.ssl_verify = false
-    config.timeout = 60
-    config.application = ENV.fetch('VAULT_APP_NAME', 'peatio')
-  end
+Vault::Rails.configure do |config|
+  config.enabled = Rails.env.production?
+  config.address = ENV.fetch('VAULT_ADDR', 'http://127.0.0.1:8200')
+  config.token = ENV.fetch('VAULT_TOKEN')
+  config.ssl_verify = false
+  config.timeout = 60
+  config.application = ENV.fetch('VAULT_APP_NAME', 'peatio')
+end
 
+if ENV['VAULT_TOKEN'].to_s != ''
   def renew_process
     token = Vault.auth_token.lookup(Vault.token)
     time = token.data[:ttl] * (1 + rand) * 0.1
@@ -39,9 +39,5 @@ if ENV['VAULT_TOKEN'].to_s != ''
     Rails.logger.info '[VAULT] Token is not renewable'
   end
 else
-  if Rails.env.production?
-    raise 'Environment variable VAULT_TOKEN is missing'
-  else
-    Rails.logger.warn 'Environment variable VAULT_TOKEN is missing'
-  end
+  Rails.logger.warn 'Environment variable VAULT_TOKEN is missing'
 end
