e0d69c5d0650ddaccb4fbcce3e71c7176ed62c20
Danil Pismenny <danil@brandymint.ru>
2021-03-04 17:12:45 +0300

    Enhancement: Combine all registries into one file

--- config/initializers/blockchain_api.rb
+++
@@ -1,3 +0,0 @@
-Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain
-Peatio::Blockchain.registry[:geth] = Ethereum::Blockchain
-Peatio::Blockchain.registry[:parity] = Ethereum::Blockchain
--- config/initializers/registries.rb
+++ config/initializers/registries.rb
@@ -1,3 +1,5 @@
+# Wallets
+
 Peatio::Wallet.registry[:bitcoind] = Bitcoin::Wallet
 Peatio::Wallet.registry[:geth] = Ethereum::Wallet
 Peatio::Wallet.registry[:parity] = Ethereum::Wallet
@@ -5,3 +7,14 @@ Peatio::Wallet.registry[:gnosis] = Gnosis::Wallet
 Peatio::Wallet.registry[:ow_hdwallet] = OWHDWallet::Wallet
 Peatio::Wallet.registry[:opendax] = OWHDWallet::Wallet
 Peatio::Wallet.registry[:opendax_cloud] = OpendaxCloud::Wallet
+
+# Blockchains
+
+Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain
+Peatio::Blockchain.registry[:geth] = Ethereum::Blockchain
+Peatio::Blockchain.registry[:parity] = Ethereum::Blockchain
+
+
+# Upstreams
+require 'peatio/upstream/opendax'
+Peatio::Upstream.registry[:opendax] = Peatio::Upstream::Opendax
