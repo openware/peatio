9ea101306f22f1b9c05022bb59ed83be8f250abb
Danil Pismenny <danil@brandymint.ru>
2021-03-04 17:21:41 +0300

    Enhancement: Move registries into config/registries.yml

--- config/initializers/registries.rb
+++ config/initializers/registries.rb
@@ -1,20 +1,16 @@
-# Wallets
+registries = {
+  wallets: Peatio::Wallet.registry,
+  blockchains: Peatio::Blockchain.registry,
+  upstreams: Peatio::Upstream.registry
+}
 
-Peatio::Wallet.registry[:bitcoind] = Bitcoin::Wallet
-Peatio::Wallet.registry[:geth] = Ethereum::Wallet
-Peatio::Wallet.registry[:parity] = Ethereum::Wallet
-Peatio::Wallet.registry[:gnosis] = Gnosis::Wallet
-Peatio::Wallet.registry[:ow_hdwallet] = OWHDWallet::Wallet
-Peatio::Wallet.registry[:opendax] = OWHDWallet::Wallet
-Peatio::Wallet.registry[:opendax_cloud] = OpendaxCloud::Wallet
-
-# Blockchains
-
-Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain
-Peatio::Blockchain.registry[:geth] = Ethereum::Blockchain
-Peatio::Blockchain.registry[:parity] = Ethereum::Blockchain
-
-
-# Upstreams
-require 'peatio/upstream/opendax'
-Peatio::Upstream.registry[:opendax] = Peatio::Upstream::Opendax
+%w(config/registries.local.yml config/registries.yml).each do |config_file|
+  next unless File.exists? config_file
+  YAML.load_file(config_file).each_pair do |registry_name, items|
+    registry = registries[registry_name.to_sym] || raise("Unknown registry (#{registry_name}) defined in #{config_file}")
+    items.each do |item_name, item_class|
+      registry[item_name.to_sym] = item_name.classify
+    end
+  end
+  break
+end
